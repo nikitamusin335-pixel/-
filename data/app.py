fd611ae65ad8a0ce067fe36adf9c9de7ffb8e91b
nikitamusin335@gmail.com <SYSADM@itcube.local>
2026-04-16 18:22:59 +0500

    актуализация общей структуры / обновления Docher

--- data/app.py
+++ data/app.py
@@ -1,38 +1,38 @@
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMIddleware
+from fastapi import FastAIP, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 import httpx
 import json
 
-app = FastAPI()
+app = FastAIP()
 
 app.add_middleware(
-    CORSMIddleware,
-    allow_orgins=["*"],
-    allow_credentials=True,
-    allown_methods=["*"],
-    allown_headers=["*"],
+    CORSMiddleware,
+    allow_origis=["*"],
+    allow_credentials=True
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.mount("/static", StaticFiles(directory="static"), name = "static")
 
 OLLAMA_URL = "http://localhost:11434/api/chat"
-MODEL_NAME = "GEMMA3:4b"
+MODEL_NAME = "gemma3:4b"
 
 @app.post("/chat")
 async def chat(request: Request):
     data = await request.json()
     user_message = data.get("message")
     history = data.get("history", [])
-
+    
     message = history + [{"role": "user", "content: user_message"}]
 
     async def generate():
         async with httpx.AsyncClient() as client:
             async with client.stream(
                 "POST",
-                OLLMA_URL,
-                json={"model": MODEL_NAME, "message": messages, "stream":True},
+                OLLAMA_URL,
+                JSON={"model": MODEL_NAME, "message": messages, "stream": True},
                 timeout = 60.0
             ) as response:
                 async for line in response.aiter_lines():
@@ -41,3 +41,6 @@ async def chat(request: Request):
 
     return StreamingResponse(generate(), media_type = "test/event-stream")
 
+    if__name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
